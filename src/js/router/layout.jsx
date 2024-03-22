a92ecb08cd4686b63ee38de8fadd593ad107e693
ElioAloise <155248934+ElioAloise@users.noreply.github.com>
2024-03-22 21:06:22 +0000

    last commit

--- src/js/router/layout.jsx
+++ src/js/router/layout.jsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react'
 import Home from "../component/home"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TodoList from '../component/TodoList';
-import Login from '../component/Login';
+
 
 
 function Layout() {
@@ -18,7 +18,6 @@ function Layout() {
         <Routes>
             <Route element={<Home setName={setName} name={name}/>} path='/'/>
             <Route element={<TodoList name={name}/>} path="/list"/>
-            <Route element={<Login/>} path="/login"/>
             <Route element={<h1>404 Page not found</h1>} path='/*'/>
         </Routes>
      </BrowserRouter>
